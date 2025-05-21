1e49f27062ffa9dd6d074a229b7eb55f0c3d469f
Markus Hughes <markushughes11@gmail.com>
2025-05-21 18:40:11 +0100

    style(main.tf): Comment out Azure client config and module definitions for clarity

--- stacks/landing_zone/main.tf
+++ stacks/landing_zone/main.tf
@@ -1,11 +1,11 @@
-data "azurerm_client_config" "current" {}
+# data "azurerm_client_config" "current" {}
 
-module "avm-ptn-alz" {
-  source  = "Azure/avm-ptn-alz/azurerm"
-  version = "0.12.0"
+# module "avm-ptn-alz" {
+#   source  = "Azure/avm-ptn-alz/azurerm"
+#   version = "0.12.0"
 
-  location = "uksouth"
-  architecture_name = "alz"
-  parent_resource_id = data.azurerm_client_config.current.tenant_id
-}
+#   location = "uksouth"
+#   architecture_name = "alz"
+#   parent_resource_id = data.azurerm_client_config.current.tenant_id
+# }
 
