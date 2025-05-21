169aa2be2b58b06e1f5c7abe2bfd260ee0ebb4c3
Markus Hughes <markushughes11@gmail.com>
2025-05-21 17:54:19 +0100

    chore(stacks): Remove generated Terraform and stack configuration files for virtual networking

--- stacks/landing_zone/_generated_backend.tf
+++ stacks/landing_zone/_generated_backend.tf
@@ -3,7 +3,7 @@
 terraform {
   backend "azurerm" {
     container_name       = "tfstate"
-    key                  = "terraform/stacks/by-id/70bb5cf6-103a-4658-89cd-23ddfb4d8aa9/terraform.tfstate"
+    key                  = "terraform/stacks/by-id/4b70644a-b384-4421-86f9-412ca4f1bbf9/terraform.tfstate"
     resource_group_name  = "rg-mbh-homelab"
     storage_account_name = "sambhhomelab"
     use_oidc             = true
---
+++ stacks/landing_zone/stack.tm.hcl
@@ -0,0 +1,5 @@
+stack {
+  name        = "landing_zone"
+  description = "landing_zone"
+  id          = "4b70644a-b384-4421-86f9-412ca4f1bbf9"
+}
